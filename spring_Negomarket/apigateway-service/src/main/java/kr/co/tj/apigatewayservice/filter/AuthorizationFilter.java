2e9edbbbe17bb9d9449669988ed118a4dff3deef
tj705 <qwer4667@naver.com>
2023-08-28 12:48:05 +0900

    test.cicd

--- spring_Negomarket/apigateway-service/src/main/java/kr/co/tj/apigatewayservice/filter/AuthorizationFilter.java
+++ spring_Negomarket/apigateway-service/src/main/java/kr/co/tj/apigatewayservice/filter/AuthorizationFilter.java
@@ -91,7 +91,6 @@ public class AuthorizationFilter extends AbstractGatewayFilterFactory<Authorizat
 		return isValid;
 	}
 
-
 	private Mono<Void> onError(ServerWebExchange exchange, String string, HttpStatus status) {
 		ServerHttpResponse response =  exchange.getResponse();
 		response.setStatusCode(status);
