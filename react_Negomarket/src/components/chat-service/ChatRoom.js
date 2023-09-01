0ecf91003bcf24fec30c907a5f9d4f5a11f7eda0
tj705 <qwer4667@naver.com>
2023-09-01 11:45:36 +0900

    react

--- react_Negomarket/src/components/chat-service/ChatRoom.js
+++ react_Negomarket/src/components/chat-service/ChatRoom.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Client } from "@stomp/stompjs";
 import React, { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
